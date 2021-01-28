4a503a781f916415253e86240e8d2d4b2db10921
Ilia Zaraysky <nextontext@yandex.ru>
2021-01-28 17:20:09 +0300

    Passed 13 of 20 tests

--- homework.py
+++ homework.py
@@ -12,30 +12,23 @@ class Calculator:
 
     def get_today_stats(self):
         now = dt.datetime.now()
-
-        today_amount = 0
         for data in self.records:
-            if data.date == now.date():
-                today_amount += data.amount
+            convert_date = dt.datetime.strptime(data.date, '%d.%m.%Y')
+            if convert_date.date() == now.date():
+                self.limit -= data.amount
             else:
                 pass
-        return today_amount
+        return self.limit
 
     def get_today_cash_remained(self, currency):
-        now = dt.datetime.now()
-
-        today_amount = 0
-        for data in self.records:
-            if data.date == now.date():
-                today_amount += data.amount
-            else:
-                pass
-        if today_amount == 0:
-            print("Денег нет, держись")
-        elif self.limit < today_amount:
-            print(f"Денег нет, держись: твой долг - {self.limit - today_amount} {currency}")
-        elif self.limit > today_amount:
-            print(f"На сегодня осталось {self.limit - today_amount} {currency}")
+        Calculator.get_today_stats()
+        if self.limit == 0:
+            convert = "Денег нет, держись"
+        elif self.limit > 0:
+            convert = f"На сегодня осталось {round(self.limit, 2)} {currency}"
+        else:
+            convert = f"Денег нет, держись: твой долг - {round(self.limit, 2)} {self.currency}"
+        return convert
 
     def get_week_stats(self):
         now = dt.datetime.now()
@@ -53,36 +46,72 @@ class Calculator:
         return today_amount
 
     def get_calories_remained(self):
-        now = dt.datetime.now()
-
-        today_amount = 0
-        for data in self.records:
-            if data.date == now.date():
-                today_amount += data.amount
-            else:
-                pass
-
-        if self.limit > today_amount:
-            calorie_difference = self.limit - today_amount
-            print(f"Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более {calorie_difference} кКал")
+        Calculator.get_today_stats()
+        if self.limit > 0:
+            print(f"Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более {self.limit} кКал")
         else:
             print("Хватит есть!")
 
-
 class Record:
-    now = dt.datetime.now()
 
-    def __init__(self, amount, comment, date=now.date()):
+    def __init__(self, amount, comment, date=None):
         self.amount = amount
-        self.date = date
+        self.date = date or dt.datetime.now()
         self.comment = comment
 
 
 class CaloriesCalculator(Calculator):
-    pass
+    def get_today_stats(self):
+        return Calculator.get_today_stats(self)
+
+    def get_calories_remained(self):
+        CaloriesCalculator.get_today_stats(self)
+        # print("Каллорийность", self.limit)
+        if self.limit > 0:
+            print(f"Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более {self.limit} кКал")
+        else:
+            print("Хватит есть!")
 
 
 class CashCalculator(Calculator):
+    USD_RATE = 75.11
+    EURO_RATE = 90.11
 
-    USD_RATE = 75.00
-    EURO_RATE = 90.00
+    def __init__(self, limit):
+        super().__init__(limit)
+        self.currency = "руб"
+
+    def get_today_stats(self):
+        return Calculator.get_today_stats(self)
+
+    def get_today_cash_remained(self, currency):
+        self.currency = currency
+        CashCalculator.get_today_stats(self)
+        convert = 0
+        if self.currency == "usd":
+            convert = self.limit / CashCalculator.USD_RATE
+            self.currency = "USD"
+        elif self.currency == "eur":
+            convert = self.limit / CashCalculator.EURO_RATE
+            self.currency = "Euro"
+        elif self.currency == "rub":
+            convert = self.limit
+            self.currency = "руб"
+
+        if self.limit == 0:
+            answer = "Денег нет, держись"
+        elif self.limit > 0:
+            answer = f"На сегодня осталось {abs(round(convert,2))} {self.currency}"
+        else:
+            answer = f"Денег нет, держись: твой долг - {abs(round(convert, 2))} {self.currency}"
+        return answer
+
+
+cash_calculator = CashCalculator(1000)
+calories_calculator = CaloriesCalculator(1000)
+cash_calculator.add_record(Record(amount=1110, comment="кофе", date='28.01.2021'))
+calories_calculator.add_record(Record(amount=900, comment="Кофе", date='28.01.2021'))
+# print(cash_calculator.get_today_cash_remained("rub"))
+print(cash_calculator.get_today_cash_remained("rub"))
+print(calories_calculator.get_calories_remained())
+# print(cash_calculator.get_today_cash_remained("eur"))
